cd9a7ac3550766148dd1a59a860e77eb41edf970
kelvin Githinji <githinjikelvin74@gmail.com>
2025-05-07 11:44:07 +0300

    initial commit

--- loop.py
+++ loop.py
@@ -4,6 +4,7 @@ import random
 secret_number = random.randint(1, 100)
 guess = None
 attempts = 0
+max_attempts = 5
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
@@ -11,15 +12,18 @@ print("I'm thinking of a number between 1 and 100.")
 # While loop to keep asking until the user guesses correctly
 while guess != secret_number:
     try:
-        guess = int(input("Enter your guess: "))
-        attempts += 1
-
-        if guess < secret_number:
-            print("Too low! Try again.")
-        elif guess > secret_number:
-            print("Too high! Try again.")
+        if max_attempts == attempts:
+            break 
         else:
-            print(f"Congratulations! You guessed it in {attempts} attempts.")
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed it in {attempts} attempts.")
     except ValueError:
         print("Please enter a valid number.")
 
