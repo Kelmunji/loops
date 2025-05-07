3dfb6cfb807a96daf7e3906b0fed158be07b37ee
shirocodes <ishirapt@gmail.com>
2025-05-07 06:50:59 +0300

    Added the for loop scenario.)

---
+++ for.py
@@ -0,0 +1,12 @@
+
+# scenario 1: FOR LOOP
+ratings = [7, 8, 10, 9, 6]
+percentages = []
+for rating in ratings:
+    percentage = str(rating *10) + "%"
+    percentages.append(percentage)
+
+print(percentages)
+
+# Scenario 2: LIST COMPREHENSION
+
